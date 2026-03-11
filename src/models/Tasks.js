cab59c69822961f38dabd9da3353f9712e3d37a2
caetanaldo <gustavocaetanodalponte@gmail.com>
2026-03-11 08:36:24 -0300

    tabela Tasks criada

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -1,3 +1,17 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database/sqlConnection.js";
 
+const Tasks = sequelize.define("Tasks",{
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description:{
+        type: DataTypes.STRING
+    },
+    completed:{
+        type: DataTypes.BOOLEAN   
+    }
+})
+
+export default Tasks
