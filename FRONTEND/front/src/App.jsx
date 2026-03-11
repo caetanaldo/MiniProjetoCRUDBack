fd9c2797d0ca5cc67aed5aaf367e594adc75a166
caetanaldo <gustavocaetanodalponte@gmail.com>
2026-03-11 11:25:43 -0300

    TaskForm fazido

---
+++ FRONTEND/front/src/App.jsx
@@ -0,0 +1,13 @@
+import TaskForm from "./components/TaskForm"
+
+
+function App() {
+
+  return (
+    <div>
+      <TaskForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ FRONTEND/front/src/components/TaskForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { api } from "../services/api";
+
+export default function TaskForm() {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await api.post("/", {
+            title,
+            description,
+            completed: false
+        })
+
+        setTitle("")
+        setDescription("")
+
+        loadTasks()
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="Título"
+                value={title}
+                onChange={(e)=> setTitle(e.target.value)}
+            />
+
+            <input
+                placeholder="Descrição"
+                value={description}
+                onChange={(e)=> setDescription(e.target.value)}
+            />
+
+            <button>Criar Task</button>
+        </form>
+    )
+}
